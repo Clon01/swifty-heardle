cb7677540cc724a0a37a0caffc7028ac754c54e2
Glitch (glitch-hello-react) <none>
2023-05-03 11:46:34 +0000

    🐣🚆 Checkpoint ./src/constants/index.ts:57405535/107 ./src/constants/playTimes.ts:57405535/65 ./src/constants/songs.ts:57405535/4465 ./src/constants/theme.ts:57405535/186 ./src/helpers/index.ts:57405535/141 ./src/helpers/scoreToEmoji.ts:57405535/873 ./src/helpers/searchSong.ts:57405535/555 ./src/helpers/todaySolution.ts:57405535/413 ./src/types/guess.ts:57405535/141 ./src/types/song.ts:57405535/81 ./src/types/modules/Theme.d.ts:57405535/194 ./src/app.jsx:57405535/5733 ./src/types/app.styled.ts:57405535/280 ./src/index.css:57405535/399 ./src/index.jsx:57405535/965 ./index.html:57405535/2 ./vite.config.js:57405535/8 ./package.json:57405535/263

---
+++ src/constants/songs.ts
@@ -0,0 +1,202 @@
+export const songs = [
+  {
+    artist: "Taylor Swift",
+    name: "Lavender Haze",
+    youtubeId: "h8DLofLM7No",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "Bejeweled",
+    youtubeId: "b7QlX3yR2xs",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "Anti-Hero",
+    youtubeId: "b1kbLwvqugk",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "The Man",
+    youtubeId: "AqAJLh9wuZ0",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "You Need To Calm Down",
+    youtubeId: "Dkk9gvTmCXY",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "Bad Blood ft. Kendrick Lamar",
+    youtubeId: "QcIy9NiNbmo",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "Don/'t Blame Me",
+    youtubeId: "kRJKB291Z1g",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "All Of The Girls You Loved Before",
+    youtubeId: "HO-31CVnLes",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "Cruel Summer",
+    youtubeId: "aC9HkZW2hZk",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "Lover",
+    youtubeId: "uLL2xTK35Qc",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "State Of Grace (Taylor's Version)",
+    youtubeId: "-mrC5tRkxrY",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "Red (Taylor's Version)",
+    youtubeId: "R_rUYuFtNO4",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "Treacherous (Taylor's Version)",
+    youtubeId: "u1D1AgDfreg",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "I Knew You Were Trouble (Taylor's Version)",
+    youtubeId: "TqAollrUJdA",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "All Too Well (Taylor's Version)",
+    youtubeId: "9OQBDdNHmXo",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "22 (Taylor's Version)",
+    youtubeId: "9boiT64sm0Q",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "I Almost Do (Taylor's Version)",
+    youtubeId: "w1AV_35zVwU",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "We Are Never Ever Getting Back Together (Taylor's Version)",
+    youtubeId: "zJFcr1KyFqE",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "Stay Stay Stay (Taylor's Version)",
+    youtubeId: "OhJ-S9Nrh7Q",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "The Last Time (Taylor's Version)",
+    youtubeId: "pCH4QrSx2Jg",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "Holy Ground (Taylor's Version)",
+    youtubeId: "S4PuN-IWi2g",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "Sad Beautiful Tragic (Taylor's Version)",
+    youtubeId: "jQfB4Gahi3I",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "The Lucky One (Taylor's Version)",
+    youtubeId: "4LtQxA_ooLk",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "Everything Has Changed (Taylor's Version)",
+    youtubeId: "eMcMbWl0fDk",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "Starlight (Taylor's Version)",
+    youtubeId: "lPvcwgEuKTg",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "Begin Again (Taylor's Version)",
+    youtubeId: "dXNZaHuKWNA",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "The Moment I Knew (Taylor's Version)",
+    youtubeId: "LmXn6BU16e0",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "Come Back...Be Here (Taylor's Version)",
+    youtubeId: "hHWOAUjnmjQ",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "Girl At Home (Taylor's Version)",
+    youtubeId: "UNckfN9upqo",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "State Of Grace (Acoustic Version)",
+    youtubeId: "D3mJe28un4M",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "Ronan (Taylor's Version)",
+    youtubeId: "kdiBc40gW7s",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "Better Man (Taylor's Version)",
+    youtubeId: "PReSQYTFvcs",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "Nothing New (Taylor's Version)",
+    youtubeId: "m3fWCRvz5JA",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "Babe (Taylor's Version)",
+    youtubeId: "3pj39qZZYoQ",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "Message In A Bottle (Taylor's Version) (From The Vault)",
+    youtubeId: "cVaG6adE2mA",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "I Bet You Think About Me (Taylor's Version)",
+    youtubeId: "5UMCrq-bBCg",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "Forever Winter (Taylor's Version) (From The Vault)",
+    youtubeId: "TkAomsYFsJw",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "Run (Taylor's Version) (From The Vault)",
+    youtubeId: "flv8AEWrRMI",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "The Very First Night (Taylor's Version) (From The Vault)",
+    youtubeId: "rVuyi-dPMIc",
+  },
+  {
+    artist: "Taylor Swift",
+    name: "All Too Well: The Short Film",
+    youtubeId: "tollGa3S0o8",
+  },
+];
